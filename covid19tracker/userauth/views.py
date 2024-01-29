f1eaa598e7edeeb725b01472407f2f711ff7bee4
dennisnderitu254 <dknderitu@gmail.com>
2024-01-29 11:53:40 +0300

    Addin Authentication Feature - Sign Up

---
+++ covid19tracker/userauth/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(
+                username=username,
+                password=password
+            )
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'signup.html', {'form': form})
+    else:
+        form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
